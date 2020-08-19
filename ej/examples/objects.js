ccd648f1589a088eddaebf98590d392e7c893486
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-08-19 09:32:17 +0600

    objects

---
+++ ej/examples/objects.js
@@ -0,0 +1,19 @@
+let day1 = {
+  squirrel: false,
+  events: ['work', 'touch tree', 'see', 'look']
+}
+
+console.log(day1.squirrel);
+
+let descriptions = {
+  work: 'climb tree',
+  'tree climb':'jump from the tree'
+}
+
+let anObject = {left: 1, right: 2};
+
+console.log(anObject.left);
+delete anObject.left;
+console.log(anObject.left);
+console.log('left' in anObject);
+console.log('right' in anObject);
