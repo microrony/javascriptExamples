4156706490c4274193556d8d5d823e0002171cbd
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-08-20 21:57:11 +0600

    event lister

--- examples/main.js
+++
@@ -1,9 +0,0 @@
-const ul = document.querySelector('.items');
-
-// ul.remove();
-ul.firstElementChild.textContent = 'hello';
-ul.children[1].innerText = 'rony';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
-
-const btn = document.querySelector('.btn');
-btn.style.background = 'red';
