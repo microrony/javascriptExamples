44b1988eaab41bfc0837d5aa5017b9350266f694
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-08-23 00:02:49 +0600

    get element by id

--- dom/main.js
+++ dom/main.js
@@ -1,2 +1,15 @@
-console.dir(document);
-console.log(document.all);
+// console.dir(document);
+// console.log(document.all);
+
+// console.log(document.getElementById('header-title'));
+
+// let headerTitle = document.getElementById('header-title');
+
+// console.log(headerTitle);
+
+// headerTitle.innerText = 'hello';
+// headerTitle.textContent = 'gello';
+// console.log(headerTitle.textContent);
+// headerTitle.innerHTML = '<h1> google </h1>';
+// let header = document.getElementById('main-header');
+// header.style.borderBottom = '3px solid black';
