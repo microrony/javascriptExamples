2d99501d766efa5eeb99c9b492db31fc0c7f36e7
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-08-23 16:33:24 +0600

    querySelector

--- dom/main.js
+++ dom/main.js
@@ -1,15 +1,30 @@
-// console.dir(document);
-// console.log(document.all);
+// // // // console.dir(document);
+// // // // console.log(document.all);
 
-// console.log(document.getElementById('header-title'));
+// // // // console.log(document.getElementById('header-title'));
 
-// let headerTitle = document.getElementById('header-title');
+// // // // let headerTitle = document.getElementById('header-title');
 
-// console.log(headerTitle);
+// // // // console.log(headerTitle);
 
-// headerTitle.innerText = 'hello';
-// headerTitle.textContent = 'gello';
-// console.log(headerTitle.textContent);
-// headerTitle.innerHTML = '<h1> google </h1>';
-// let header = document.getElementById('main-header');
-// header.style.borderBottom = '3px solid black';
+// // // // headerTitle.innerText = 'hello';
+// // // // headerTitle.textContent = 'gello';
+// // // // console.log(headerTitle.textContent);
+// // // // headerTitle.innerHTML = '<h1> google </h1>';
+// // // // let header = document.getElementById('main-header');
+// // // // header.style.borderBottom = '3px solid black';
+
+// // // let header = document.querySelector('#main-header');
+// // // header.style.borderBottom = '3px solid #ccc';
+// // // let li = document.querySelector('li');
+// // // li.style.color = 'red';
+// // let li = document.querySelector('li:nth-child(3)');
+// // li.style.color = 'red';
+// let title = document.querySelector('.title');
+// title.style.color = 'red';
+// console.log(title);
+let title = document.querySelectorAll('.title:nth-child(odd)');
+
+for (let i = 0; i < title.length; i++) {
+  title[i].style.color = 'red';
+}
