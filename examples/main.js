b8193a5c955aaf9e7e8af606bc3f654f2894f454
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-07 20:39:41 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -16,6 +16,8 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //   console.log(companies[i].name);
 // }
 
-companies.forEach((company) => {
-  console.log(company.category);
-});
+// companies.forEach((company) => {
+//   console.log(company.category);
+// });
+
+companies.forEach((company) => console.log(company.end));
