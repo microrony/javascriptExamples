643506d8bb4f573c8eb19197384128a6630b062b
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-08 10:30:34 +0600

    filter method

--- examples/main.js
+++ examples/main.js
@@ -12,6 +12,9 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
+let autoCompaniesName = companies.filter(company => company.category === 'Auto').map(company => company.name);
+console.log(autoCompaniesName);
+
 // for (let i = 0; i < companies.length; i++) {
 //   console.log(companies[i].name);
 // }
@@ -20,4 +23,31 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //   console.log(company.category);
 // });
 
-companies.forEach((company) => console.log(company.end));
+// companies.forEach((company) => console.log(company.end));
+
+// let salahIsFarg = [];
+// for (let i = 0; i < ages.length; i++) {
+//   if (ages[i] >= 21) {
+//     salahIsFarg.push(ages[i]);
+//   }
+// }
+// console.log(salahIsFarg);
+
+// let salahIsFarg = ages.filter((age) => age >= 21);
+// let salahIsFarg = ages.filter((age) => age > 15 && age < 44);
+// console.log(salahIsFarg);
+
+// let autoCompany = companies.filter((company) => {
+//   if (company.category === 'Auto') {
+//     return true;
+//   }
+// });
+
+// let autoCompany = companies.filter((company) => company.category === 'Auto');
+// console.log(autoCompany);
+
+// let oldCompanies = companies.filter((company) => company.start <= 1990);
+// console.log(oldCompanies);
+
+// let goodCompanies = companies.filter(company => (company.end - company.start) >= 10);
+// console.log(goodCompanies);
