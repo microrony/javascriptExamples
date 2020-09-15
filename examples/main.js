f14cdef9b070202714816774af876899295fb9fa
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-15 16:16:14 +0600

    create object

--- examples/main.js
+++ examples/main.js
@@ -96,3 +96,22 @@
 
 // console.log(mag1);
 
+// Object Of Protos
+const bookProtos = {
+  getSummary: function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+
+  getAge: function() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old`;
+  }
+};
+
+// Create Object
+const book1 = Object.create(bookProtos);
+book1.title = 'Book One';
+book1.author = 'John Doe';
+book1.year = '2020';
+
+console.log(book1);
