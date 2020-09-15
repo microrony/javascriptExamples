edeebaa0ac4b572f051dc85580040205e7acfe2e
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-15 15:50:06 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -33,19 +33,36 @@
 
 // console.log(book1);
 
-// prototypes
+// constructor
 function Book(title, author, year) {
   this.title = title;
   this.author = author;
   this.year = year;
 }
 
+// getSummary
 Book.prototype.getSummary = function() {
   return `${this.title} was written by ${this.author} in ${this.year}`;
+}
+
+// getAge
+Book.prototype.getAge = function() {
+  const years = new Date().getFullYear() - this.year;
+  return `${this.title} is ${years} years old`;
+}
+
+// revise
+Book.prototype.revise = function(thisYear) {
+  this.year = thisYear;
+  this.revised = true;
 }
 
 // Instatiate an Object
 const book1 = new Book('Book One', 'Jhone Doe', 2013);
 const book2 = new Book('Book Two', 'Jne Doe', 2016);
 
-console.log(book1.getSummary());
+console.log(book1);
+book1.revise(2020);
+
+console.log(book1);
+  
