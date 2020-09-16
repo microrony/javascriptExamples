d731dbe59b52f59eb11a4dc1e0ea14b88f7e9798
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-16 11:53:33 +0600

    constructor

--- examples/main.js
+++ examples/main.js
@@ -1,20 +1,34 @@
-const book1 = {
-  title: 'Book One',
-  author: 'Jhon Doe',
-  year: '2020',
-  getSammary: function () {
-    return `${this.title} was written by ${this.author} in ${this.year}`;
-  },
-};
+// const book1 = {
+//   title: 'Book One',
+//   author: 'Jhon Doe',
+//   year: '2020',
+//   getSammary: function () {
+//     return `${this.title} was written by ${this.author} in ${this.year}`;
+//   },
+// };
 
-const book2 = {
-  title: 'Book Two',
-  author: 'Jhon Doe',
-  year: '2019',
-  getSammary: function () {
-    return `${this.title} was written by ${this.author} in ${this.year}`;
-  },
-};
+// const book2 = {
+//   title: 'Book Two',
+//   author: 'Jhon Doe',
+//   year: '2019',
+//   getSammary: function () {
+//     return `${this.title} was written by ${this.author} in ${this.year}`;
+//   },
+// };
+
+// console.log(book1);
+// console.log(book1.getSammary());
+
+// Constructor
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+// Intentiate an Object
+const book1 = new Book('Book One', 'John Doe', '2020');
+const book2 = new Book('Book Two', 'John Doe', '2019');
 
 console.log(book1);
-console.log(book1.getSammary());
+// console.log(book2.getSammary());
