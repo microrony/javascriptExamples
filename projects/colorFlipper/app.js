987d63c5c1d4946cdd9abc490271bedb6d10f6aa
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-25 14:04:57 +0600

    update

--- projects/colorFlipper/app.js
+++ projects/colorFlipper/app.js
@@ -1,10 +1,17 @@
+// variables
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const body = document.body;
 
-btn.addEventListener('click', function() {
+// functions
+const flipColor = () => {
+  // get random number 0 - 3
   const randomNumber = 2;
-  document.body.style.backgroundColor = colors[randomNumber];
+  body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
-});
+}
+
+// events
+btn.addEventListener('click', flipColor);
