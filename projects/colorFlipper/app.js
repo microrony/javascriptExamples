c41df81522ca2276e768f8dea76fd8eed763a4c5
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-26 12:30:49 +0600

    update

--- projects/colorFlipper/app.js
+++ projects/colorFlipper/app.js
@@ -5,18 +5,16 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 const body = document.body;
 
-// functions
+// function
 const getRandomNumber = () => {
   return Math.floor(Math.random() * colors.length);
 }
 
 const flipColor = () => {
-  // get random number 0 - 3
+  // get randoem number 0 - 3
   const randomNumber = getRandomNumber();
-  console.log(randomNumber);
   body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 }
-
-// events
+// event 
 btn.addEventListener('click', flipColor);
