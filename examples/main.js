80239edb335fd72b4d23658bf21229e236a732eb
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-28 11:05:55 +0600

    object literal

--- examples/main.js
+++ examples/main.js
@@ -1,90 +1,20 @@
-// default paramiters
-// let getValue = function (value = 10) {
-//   console.log(value);
-// };
-
-// getValue();
-
-// rest operator
-// let displayColors = function (message, ...colors) {
-//   console.log(message);
-
-//   for (let i in colors) {
-//     console.log(colors[i]);
-//   }
-// };
-
-// let message = 'list of colors';
-
-// displayColors(message, 'red');
-// displayColors(message, 'red', 'blue');
-// displayColors(message, 'red', 'blue', 'green');
-
-// spread operator
-// let displayColors = function (note, ...colors) {
-//   console.log(note);
-
-//   for (let i in colors) {
-//     console.log(colors[i]);
-//   }
-// };
-
-// let note = 'learning rest oparator';
-
-// let colorsArray = ['orange', 'blue', 'red'];
-// displayColors(note, ...colorsArray);
-
-// let displayColors = (note, ...colors) => {
-//   console.log(note);
-
-//   for (let i in colors) {
-//     console.log(colors[i]);
-//   }
-// };
-
-// let note = 'learning spread operator';
-
-// let colorsArray = ['orange', 'red', 'blue', 'yellow'];
-// displayColors(note, ...colorsArray);
-
-// object literals
-// let firstName = 'mahabubur';
-// let lastName = 'rahman';
-
-// let person = {
-//   firstName,
-//   lastName,
-// };
-
-// let createPerson = (firstName, lastName, age) => {
-//   let fullName = `${firstName} ${lastName}`;
-//   return {
-//     firstName,
-//     lastName,
-//     fullName,
-//     isSenior() {
-//       return age > 18;
-//     },
-//   };
-// };
-
-// let p = createPerson('mahabubur', 'rahman', 20);
-// console.log(p.firstName);
-// console.log(p.lastName);
-// console.log(p.fullName);
-// console.log(p.isSenior());
-// console.log(person);
-
-// destructuring array
-// let me = ['mahabubur', 'rahman', 'male'];
-
-let me = {
-  fname: 'mahabubur',
-  lname: 'rahman',
-  gender: 'male',
+// object literal
+const book1 = {
+  title: 'Book One',
+  year: '2020',
+  author: 'John Doe',
+  getSummary() {
+    return `${this.title} is written by ${this.author} in ${this.year}`;
+  },
 };
 
-let { fname, lname, gender } = me;
-console.log(fname);
-console.log(lname);
-console.log(gender);
+const book2 = {
+  title: 'Book Two',
+  year: '2019',
+  author: 'John Doe',
+  getSummary() {
+    return `${this.title} is written by ${this.author} in ${this.year}`;
+  },
+};
+
+console.log(book2.getSummary());
