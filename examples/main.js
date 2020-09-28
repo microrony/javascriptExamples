f0526393b68e741237c8455297cd12a06e8f8f91
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-28 12:58:36 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -151,6 +151,28 @@
 
 // console.log(mag1);
 
+// // Object of protos
+// const bookProtos = {
+//   getSummary: function () {
+//     return `${this.title} is written by ${this.author} in ${this.year}`;
+//   },
+// };
+
+// // // Create object
+// // const book1 = Object.create(bookProtos);
+// // book1.title = 'Book One';
+// // book1.author = 'John Doe';
+// // book1.year = '2020';
+
+// // another way of doing this
+// const book1 = Object.create(bookProtos, {
+//   title: { value: 'Book One' },
+//   author: { value: 'John Doe' },
+//   year: { value: '2020' },
+// });
+
+// console.log(book1);
+
 // Object of protos
 const bookProtos = {
   getSummary: function () {
@@ -158,9 +180,9 @@ const bookProtos = {
   },
 };
 
-// // Create object
+// // create object
 // const book1 = Object.create(bookProtos);
-// book1.title = 'Book One';
+// book1.tilte = 'Book One';
 // book1.author = 'John Doe';
 // book1.year = '2020';
 
