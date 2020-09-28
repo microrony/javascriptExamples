85ffa0833122fcfadbcf5d2c286e69046b3c6b3c
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-28 11:45:34 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -53,21 +53,40 @@
 // console.log(book1);
 // console.log(book2.getSummary());
 
-//constructor
+// //constructor
+// function Book(title, author, year) {
+//   this.title = title;
+//   this.author = author;
+//   this.year = year;
+// }
+
+// //ptototypes
+// Book.prototype.getSummary = function () {
+//   return `${this.title} is written by ${this.author} in ${this.year}`;
+// };
+
+// //instantiate an object
+// const book1 = new Book('Book One', 'John Doe', '2020');
+// const book2 = new Book('Book Two', 'John Doe', '2019');
+
+// console.log(book1);
+// console.log(book2.getSummary());
+
+// constructor
 function Book(title, author, year) {
   this.title = title;
   this.author = author;
   this.year = year;
 }
 
-//ptototypes
+// prototype
 Book.prototype.getSummary = function () {
   return `${this.title} is written by ${this.author} in ${this.year}`;
 };
 
-//instantiate an object
-const book1 = new Book('Book One', 'John Doe', '2020');
-const book2 = new Book('Book Two', 'John Doe', '2019');
+// instantiate an object
+const book1 = new Book('Book One', 'John Doe', 2020);
+const book2 = new Book('Book Two', 'John Doe', 2019);
 
 console.log(book1);
 console.log(book2.getSummary());
