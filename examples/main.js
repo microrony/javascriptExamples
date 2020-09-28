ea6916b6c202cda5ba47ee84a9c704a3363f4f17
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-28 12:52:54 +0600

    create object

--- examples/main.js
+++ examples/main.js
@@ -121,32 +121,54 @@
 
 // console.log(mag1);
 
-// constructor
-function Book(title, author, year) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
-}
+// // constructor
+// function Book(title, author, year) {
+//   this.title = title;
+//   this.author = author;
+//   this.year = year;
+// }
 
-// prototype
-Book.prototype.getSummary = function () {
-  return `${this.title} is written by ${this.author} in ${this.year}`;
+// // prototype
+// Book.prototype.getSummary = function () {
+//   return `${this.title} is written by ${this.author} in ${this.year}`;
+// };
+
+// // Magazine constructor
+// function Magazine(title, author, year, month) {
+//   Book.call(this, title, author, year);
+
+//   this.month = month;
+// }
+
+// // inherit prototype
+// Magazine.prototype = Object.create(Book.prototype);
+
+// //use magazine constructor
+// Magazine.prototype.constructor = Magazine;
+
+// // instantiate magazine object
+// const mag1 = new Magazine('Magazine One', 'John Doe', '2020', 'Jan');
+
+// console.log(mag1);
+
+// Object of protos
+const bookProtos = {
+  getSummary: function () {
+    return `${this.title} is written by ${this.author} in ${this.year}`;
+  },
 };
 
-// Magazine constructor
-function Magazine(title, author, year, month) {
-  Book.call(this, title, author, year);
+// // Create object
+// const book1 = Object.create(bookProtos);
+// book1.title = 'Book One';
+// book1.author = 'John Doe';
+// book1.year = '2020';
 
-  this.month = month;
-}
+// another way of doing this
+const book1 = Object.create(bookProtos, {
+  title: { value: 'Book One' },
+  author: { value: 'John Doe' },
+  year: { value: '2020' },
+});
 
-// inherit prototype
-Magazine.prototype = Object.create(Book.prototype);
-
-//use magazine constructor
-Magazine.prototype.constructor = Magazine;
-
-// instantiate magazine object
-const mag1 = new Magazine('Magazine One', 'John Doe', '2020', 'Jan');
-
-console.log(mag1);
+console.log(book1);
