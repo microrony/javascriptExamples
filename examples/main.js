48789ac4bb6241d80400df6c9710d4608f003385
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-28 14:18:40 +0600

    classes

--- examples/main.js
+++ examples/main.js
@@ -173,24 +173,48 @@
 
 // console.log(book1);
 
-// Object of protos
-const bookProtos = {
-  getSummary: function () {
+// // Object of protos
+// const bookProtos = {
+//   getSummary: function () {
+//     return `${this.title} is written by ${this.author} in ${this.year}`;
+//   },
+// };
+
+// // // create object
+// // const book1 = Object.create(bookProtos);
+// // book1.tilte = 'Book One';
+// // book1.author = 'John Doe';
+// // book1.year = '2020';
+
+// // another way of doing this
+// const book1 = Object.create(bookProtos, {
+//   title: { value: 'Book One' },
+//   author: { value: 'John Doe' },
+//   year: { value: '2020' },
+// });
+
+// console.log(book1);
+
+// classes
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+  getSummary() {
     return `${this.title} is written by ${this.author} in ${this.year}`;
-  },
-};
+  }
 
-// // create object
-// const book1 = Object.create(bookProtos);
-// book1.tilte = 'Book One';
-// book1.author = 'John Doe';
-// book1.year = '2020';
+  static topBookStore() {
+    return 'Barnes & Noble';
+  }
+}
 
-// another way of doing this
-const book1 = Object.create(bookProtos, {
-  title: { value: 'Book One' },
-  author: { value: 'John Doe' },
-  year: { value: '2020' },
-});
+// Instantiate an object
+const book1 = new Book('Book One', 'John Doe', '2020');
+const book2 = new Book('Book Two', 'John Doe', '2019');
 
 console.log(book1);
+console.log(book2.getSummary());
+console.log(Book.topBookStore());
