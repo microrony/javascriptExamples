aa0707821682652a73b5f4ebfc4f1ff5ae86e1ed
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-28 12:12:51 +0600

    inherit prototype

--- examples/main.js
+++ examples/main.js
@@ -72,6 +72,25 @@
 // console.log(book1);
 // console.log(book2.getSummary());
 
+// // constructor
+// function Book(title, author, year) {
+//   this.title = title;
+//   this.author = author;
+//   this.year = year;
+// }
+
+// // prototype
+// Book.prototype.getSummary = function () {
+//   return `${this.title} is written by ${this.author} in ${this.year}`;
+// };
+
+// // instantiate an object
+// const book1 = new Book('Book One', 'John Doe', 2020);
+// const book2 = new Book('Book Two', 'John Doe', 2019);
+
+// console.log(book1);
+// console.log(book2.getSummary());
+
 // constructor
 function Book(title, author, year) {
   this.title = title;
@@ -79,14 +98,25 @@ function Book(title, author, year) {
   this.year = year;
 }
 
-// prototype
+// ptototype
 Book.prototype.getSummary = function () {
   return `${this.title} is written by ${this.author} in ${this.year}`;
 };
 
-// instantiate an object
-const book1 = new Book('Book One', 'John Doe', 2020);
-const book2 = new Book('Book Two', 'John Doe', 2019);
+// magazine constructor
+function Magazine(title, author, year, month) {
+  Book.call(this, title, author, year);
 
-console.log(book1);
-console.log(book2.getSummary());
+  this.month = month;
+}
+
+// inherit prototype
+Magazine.prototype = Object.create(Book.prototype);
+
+// instantiate magazine object
+const mag1 = new Magazine('Magazine One', 'John Doe', '2020', 'Jan');
+
+// use magazine constructor
+Magazine.prototype.constructor = Magazine;
+
+console.log(mag1);
