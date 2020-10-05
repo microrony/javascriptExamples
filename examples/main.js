6c583e0914ebe8c0ea9cd72e91f40f5efb8a63cc
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-05 15:16:57 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -12,5 +12,5 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-const canDrink = ages.filter(age => age >= 21);
-console.log(canDrink);
+const retailCompanies = companies.filter(company => company.category === 'Retail');
+console.log(retailCompanies);
