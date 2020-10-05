ae68f7a5a3af26d1c087c360356345bba687e60d
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-05 15:35:01 +0600

    reduce

--- examples/main.js
+++ examples/main.js
@@ -12,5 +12,5 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-const sortAges = ages.sort((a,b) => a - b);
-console.log(sortAges);
+const sumAges = ages.reduce((a, b) => a + b, 0)
+console.log(sumAges);
