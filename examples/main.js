8de74d6455bc22009b18a0e1769a1161c1e2a84a
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-05 15:20:42 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -12,5 +12,6 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-const retailCompanies = companies.filter(company => company.category === 'Retail');
-console.log(retailCompanies);
+
+const eightiesCompanies = companies.filter(company => company.start >= 1980 && company.end <= 1999);
+console.log(eightiesCompanies);
