e3d14785c3c06c79cc9a8e25b7d8c8d7eef4415a
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-05 15:28:09 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -12,5 +12,5 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-const companyName = companies.map(company => company.name);
-console.log(companyName);
+const companyD = companies.map(company => `${company.name} - [${company.start} - ${company.end}]`)
+console.log(companyD);
