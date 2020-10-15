cddf207b1832ed12b392f73012b93e6e97c3c583
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-15 14:28:38 +0600

    callback

--- examples/main.js
+++ examples/main.js
@@ -13,12 +13,11 @@ function getPosts() {
   }, 1000);
 }
 
-function createPost(post) {
+function createPost(post, callback) {
   setTimeout(() => {
     posts.push(post);
+    callback();
   }, 2000);
 }
 
-getPosts();
-
-createPost({ title: 'Post Three', body: 'This is post tree' });
+createPost({ title: 'Post Three', body: 'This is post tree' }, getPosts);
