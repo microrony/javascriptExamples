1a140576577a0de91edd4f116a652ad8af008a98
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-15 16:17:58 +0600

    asynce / await

--- examples/main.js
+++ examples/main.js
@@ -1,12 +1,38 @@
-const promise1 = Promise.resolve('Hello World');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 2000, 'Goodbye');
-});
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res =>
-  res.json()
-);
+const posts = [
+  { title: 'Post One', body: 'This is a post one' },
+  { title: 'Post Two', body: 'This is a post two' },
+];
 
-Promise.all([promise1, promise2, promise3, promise4]).then(values =>
-  console.log(values)
-);
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach(post => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPosts(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject('Error: Somthing Went Wrong');
+      }
+    }, 2000);
+  });
+}
+
+async function init() {
+  await createPosts({ title: 'Post Three', body: 'This is post three' });
+
+  getPosts();
+}
+
+init();
