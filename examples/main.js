4a70af663f468d45c67e1b3c23cbed2405094352
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-15 16:24:14 +0600

    update

--- examples/main.js
+++
@@ -1,9 +0,0 @@
-async function fetchUsers() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-
-  const data = await res.json();
-
-  console.log(data);
-}
-
-fetchUsers();
