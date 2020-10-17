dd340a1ff3c20d9f2bc6c7bf2f902d648a46a3c5
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-17 21:35:23 +0600

    update

--- laxios/main.js
+++ laxios/main.js
@@ -131,7 +131,25 @@ function transformResponse() {
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios
+    .get('https://jsonplaceholder.typicode.com/todoss')
+    .then(res => showOutput(res))
+    .catch(err => {
+      if (err.response) {
+        // Server responded with a status other than 200 range
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+
+        if (err.response.status === 404) {
+          alert('Error: Page Not Found');
+        } else if (err.request) {
+          console.error(err.request);
+        } else {
+          console.error(err.message);
+        }
+      }
+    });
 }
 
 // CANCEL TOKEN
