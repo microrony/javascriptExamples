f0dedb1af82f0be0f3d86832266c604149c9cfba
Mahabubur Rahman <microronyy@gmail.com>
2020-12-21 21:48:43 +0600

    update

--- examples/main.js
+++ examples/main.js
@@ -1,21 +1,25 @@
-const getTodos = (resource) => {
+const getTodos = (resource, callback) => {
+  const request = new XMLHttpRequest()
 
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
-
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText)
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject('error')
-      }
-    });
-  
-    request.open('GET', resource);
-    request.send()
-  })};
-
-  getTodos('Todos/raju.json').then(data => {
-    console.log(data)
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const data = JSON.parse(request.responseText)
+      callback(undefined, data);
+    } else if (request.readyState === 4) {
+      callback('could not fetch data', undefined);
+    }
   })
+
+  request.open('GET', resource)
+  request.send()
+}
+
+getTodos('Todos/rony.json', (err, data) => {
+  console.log(data);
+  getTodos('Todos/sattar.json', (err,data) => {
+    console.log(data);
+    getTodos('Todos/raju.json', (err, data) => {
+      console.log(data);
+    })
+  })
+});
