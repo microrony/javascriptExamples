67ec888ca08b09c066b94e79620c9e46bc5761f1
Mahabubur Rahman <microronyy@gmail.com>
2020-12-24 18:57:27 +0600

    asycn & await and throw error

--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,16 @@
-// Fetch API
+// async & await
 
-fetch('Todos/raju.json').then(res => {
-  return res.json();
-}).then(data => {
-  console.log(data)
-}).catch(err => {
-  console.log(err);
-})
+const getTodos = async () => {
+  const response = await fetch('Todos/raju.json');
+
+  if(response.status !== 200) {
+    throw new Error('cannot fetch the data');
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+getTodos()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
