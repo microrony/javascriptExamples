e034feae198d71be84e49651c943d3e08d70411f
Mahabubur Rahman <microronyy@gmail.com>
2021-03-18 11:55:45 +0600

    javascript

--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,26 @@
-const character = 't'
-const count = 5
-let mystring = ''
+const res = {
+  data: [
+    {
+      latitude: 2.521947,
+      longitude: 113.833569,
+      type: 'country',
+      name: 'Malaysia',
+      number: null,
+      postal_code: null,
+      street: null,
+      confidence: 1,
+      region: null,
+      region_code: null,
+      county: null,
+      locality: null,
+      administrative_area: null,
+      neighbourhood: null,
+      country: 'Malaysia',
+      country_code: 'MYS',
+      continent: 'Asia',
+      label: 'Malaysia',
+    },
+  ],
+};
 
-for (let i = 0; i < 5; i++) {
-  mystring += character
-}
-
-console.log(mystring)
+console.log(res.data[0].latitude);
