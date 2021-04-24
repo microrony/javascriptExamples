ff196b1ffb9815c8159afc006aaf41e893d5b0f8
Mahabubur Rahman <microronyy@gmail.com>
2021-04-24 16:43:58 +0600

    version 1 todo add is complete

--- practical-javascript/main.js
+++ practical-javascript/main.js
@@ -1,70 +1,9 @@
-// console.log('Hello');
-// //console.log(myvar);
-// console.error('An error');
-// console.warn('A warning');
-// console.dir(document);
+var todos = ['Item 1', 'Item 2', 'Item 3'];
 
-// console.table([{ name: 'John', email: 'test@test.com', age: 33 }])
+console.log(todos);
 
-// console.clear()
+todos.push('Item 4');
 
-// console.group('Say Hello')
-// console.log('Hello Brad')
-// console.log('Hello John')
-// console.log('Hello Sally')
-// console.groupEnd('Say Hello')
+todos[0] = 'Item 1 updated';
 
-// console.time('Run Loop');
-// for (var i = 0; i < 20000; i++) {
-//     console.log(i);
-// };
-// console.timeEnd('Run Loop');
-
-// function greaterThan(a,b) {
-//   console.assert(a > b, {"message":"a is not greater than b","a":a,"b":b});
-// }
-
-// greaterThan(5,6);
-
-// document.querySelector('button').addEventListener('click', loadUsers);
-
-//   // Load Github Users
-//   function loadUsers(){
-//      var xhr = new XMLHttpRequest();
-//       xhr.open('GET', 'https://api.github.com/users', true);
-
-//       xhr.onload = function(){
-//         if(this.status == 200){
-//           var users = JSON.parse(this.responseText);
-
-//           var output = '';
-//           for(var i in users){
-//             output +=
-//               '<div class="user">' +
-//               '<img src="'+users[i].avatar_url+'" width="70" height="70">' +
-//               '<ul>' +
-//               '<li>ID: '+users[i].id+'</li>' +
-//               '<li>Login: '+users[i].login+'</li>' +
-//               '</ul>' +
-//               '</div>';
-//           }
-
-//           document.getElementById('users').innerHTML = output;
-//         }
-//       }
-
-//       xhr.send();
-//     }
-
-// Set localstorage - will stay until delete/clear
-// localStorage.setItem('name', 'John')
-
-// Set sessionstorage - will stay until browser close
-// sessionStorage.setItem('user', 'Will')
-
-// Set cookie
-// document.cookie = 'username=John Doe'
-let rony = 'rony';
-let satter = 'sattar';
-let raju = 'raju';
-rony = 'roney';
+todos.splice(0, 1);
