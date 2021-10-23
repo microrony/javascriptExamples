8d75aa3bd6b68496a7162ac2d47aa15c9a816cc4
Mahabubur Rahman <microronyy@gmail.com>
2021-10-23 16:47:23 +0600

    OOP in JavaScript

--- examples/main.js
+++ examples/main.js
@@ -1,15 +1,26 @@
-let string = 'racecar';
+class Book {
+	constructor(title, author, year) {
+		this.title = title;
+		this.author= author;
+		this.year= year;
+	}
 
-function func(param) {
-  if (param.length === 0 || param.length === 1) {
-    return true;
-  }
-
-  if (param.charAt(0) === param.charAt(param.length - 1)) {
-    return func(param.substring(1, param.length - 1));
-  }
-
-  return false;
+	getSummary() {
+		return `${this.title} was written by ${this.author} in ${this.year}`;
+	}
 }
 
-console.log(func(string));
+class Magazine extends Book {
+	constructor(title, author, year, month) {
+		super(title, author, year);
+
+		this.month = month;
+	}
+}
+
+const book1 = new Book('Book One', 'John', '2013');
+
+console.log(book1.getSummary())
+
+const mag1 = new Magazine('Magazine One', 'Jogn', '2018', 'Jan');
+console.log(mag1.getSummary())
